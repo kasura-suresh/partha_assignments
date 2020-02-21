a767d7a9c177a626488c1b5ead76ebc6df8e238b
kasura-suresh <kasura-suresh>
2020-02-21 06:38:31 -0800

    unplanned assignment

---
+++ array_pass.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void print_array(int arr[],int n)
+{
+	int i;
+	printf("Your array is:\n");
+	for(i = 0 ; i < n ; i++)
+	{
+		printf("%d ",arr[i]);		
+	}
+}
+
+int main()
+{
+	int i,n;
+	printf("Enter the number of element:\n");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Now enter the element:\n");
+	for(i = 0 ; i < n ;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	print_array(arr,n);
+	return 0;
+}
---
+++ dataType.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+//---signed Data Types----
+    int i = -5;
+	float f = -6.3;
+	double d = -6.33;
+	char k = 'a';
+	long l = -78945612;
+//---Unsigned Data Types--
+	unsigned int usi = 56;
+	unsigned long int usli = 789456;
+	unsigned long long int uslli = 7945612369874;
+
+//---Printing the values-----	
+	printf("Integer value:%d\n",i);
+	printf("Float value:%f\n",f);
+	printf("Double value:%lf\n",d);
+	printf("Char value:%c\n",k);
+	printf("Long value:%ld\n",l);
+
+	printf("Unsigned Integer value:%u\n",usi);
+	printf("Unsigned Long Int value:%lu\n",usli);
+	printf("Unsigned Long Long Int value:%llu\n",uslli);
+	return 0;
+}
