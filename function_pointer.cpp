bc6fade2839bfa009c19cb131dce60e4ed3dc21a
iampartha23 <iampartha23@gmail.com>
2020-03-04 14:28:01 +0530

    CMakeList

---
+++ function_pointer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int show_multiple(int x,int y){
+	int result;
+	result = x*y;
+	//printf("result is:%d",result);
+	return result;
+}
+
+int main(){
+	int (*fun)(int,int),result;
+	fun = &show_multiple;
+	result =    fun(2,30);
+	cout<<"Result is: "<<result;
+	return 0;
+}
