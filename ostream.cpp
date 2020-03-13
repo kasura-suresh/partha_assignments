332f6a4dd05dbc5972244a8003edeee656a64026
iampartha23 <iampartha23@gmail.com>
2020-03-13 11:51:12 +0530

    c++ assignments

--- ostream.cpp
+++ ostream.cpp
@@ -5,28 +5,26 @@
 	
 *************************************************/
 
-#include <iostream> 
-using namespace std; 
+#include <iostream>
+using namespace std;
+class Test{
+	int x,y;
+	public:
+		int z;
+		friend void operator >> (Test& t, istream& tin){
+			tin >> t.x >> t.y >> t.z;
+		}
+		friend void operator << (Test& t, ostream& tout){
+			tout<< "Ostream and Istream is working and the values are:\n"<<endl;
+			tout<< t.x <<" "<<t.y<<" "<<t.z <<endl;
+		}
+};  
   
 int main() 
 { 
-    char opp;
-    cout<<"insert \"h\" to say hii"<<"\n";
-    cout<<"insert \"b\" to say bye"<<"\n";
-    cin.get(opp);
-    
-	switch(opp)
-	{
-		case 'h':
-			cout<<"Hi User"<<"\n";
-			break;
-		case 'b':
-			cout<<"Bye User"<<"\n";
-			break;
-		default:
-			cout<<"please make a perfect choice"<<"\n";
-			break;
-	}
-    
-	return 0;
+  Test t;
+  cout<<"Enter the value of x and y:\n";
+  t >> cin;
+  t << cout;
+  return 0;   
 } 
